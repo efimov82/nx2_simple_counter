967ce3f1c72886491e3585a252704242b25b2646
dan82 <ed@easy-sms.ru>
2017-07-24 15:33:21 +0700

    code clear

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, Directive, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CounterComponent } from './counter/counter.component';
 
 
@@ -7,24 +7,20 @@ import { CounterComponent } from './counter/counter.component';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  
+
 
 })
-export class AppComponent implements OnInit  { // implements OnInit
-  count: number = 7;
+export class AppComponent implements OnInit {
   counter1_val: number = 2;
   counter2_val: number = 6;
-  cnt1: CounterComponent;
 
   ngOnInit() {
-    this.cnt1 = new CounterComponent();
-    this.cnt1.count = 15;
   }
 
   resetAll(counter1: CounterComponent, counter2: CounterComponent) {
     counter1.resetVal();
     counter2.resetVal();
-    
-    console.log('reset()');
+
+    console.log('reset all');
   }
 }
